7d4d80dc41e2343e506168f15e93a6698b7bfcf3
watson <105619746+bashcoding@users.noreply.github.com>
2022-10-19 09:58:39 -0500

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,158 @@
+let allMusic = [
+  {
+    name: "Ya No Hay Verano",
+    artist: "Depresión Sonora",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Gasolina y Mechero",
+    artist: "Depresión Sonora",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Paradise City",
+    artist: "Guns N' Roses",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Sweet Child O' Mine",
+    artist: "Guns N' Roses",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Addicted To Chaos",
+    artist: "Megadeth",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Hail to the King",
+    artist: "Avenged Sevenfold",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Dumb",
+    artist: "Nirvana",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "METALLICA",
+    artist: "YUNG BEEF",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Chamber of Reflection",
+    artist: "Mac DeMarco",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Nightmare",
+    artist: "Avenged Sevenfold",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Can You Feel My Heart",
+    artist: "Bring Me The Horizon",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "ギミチョコ！！",
+    artist: "BABYMETAL",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Asphyxia",
+    artist: "Cö shu Nie",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "She Loves You",
+    artist: "The Beatles",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Sad but True",
+    artist: "Metalica",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Master of Puppets",
+    artist: "Metalica",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Breaking The Law",
+    artist: "Judas Priest",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Painkiller",
+    artist: "Judas Priest",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "The Man Who Sold The World",
+    artist: "Nirvana",
+    img: "music-19",
+    src: "music-19"
+  },
+    {
+    name: "Welcome To The Jungle",
+    artist: "Guns N' Roses",
+    img: "music-20",
+    src: "music-20"
+  },
+    {
+    name: "August",
+    artist: "Intelligency",
+    img: "music-21",
+    src: "music-21"
+  },
+   {
+    name: "Smells Like Teen Spirit",
+    artist: "Nirvana",
+    img: "music-22",
+    src: "music-22"
+  },
+     {
+    name: "Heart-Shaped Box",
+    artist: "Nirvana",
+    img: "music-23",
+    src: "music-23"
+  },
+     {
+    name: "You Know You're Right",
+    artist: "Nirvana",
+    img: "music-24",
+    src: "music-24"
+  },
+     {
+    name: "In Bloom",
+    artist: "Nirvana",
+    img: "music-25",
+    src: "music-25"
+  },
+     {
+    name: "Come as You Are",
+    artist: "Nirvana",
+    img: "music-26",
+    src: "music-26"
+  },
+];
